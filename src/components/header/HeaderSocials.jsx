5ccf1007b57a3b87ecde3f2d0d15f5510b7b17fc
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-05-03 22:37:08 -0400

    Header section completed

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="#linkedin" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="#github" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="#dribble" target="_blank">
+        <FiDribbble />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
